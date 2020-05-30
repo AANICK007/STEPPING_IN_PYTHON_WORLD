253c9820663823ff49bc9f7a224ad329cb018e12
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-30 16:28:15 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
+# This is the first basic programme of python language
 print("Hello World")
+input("press enter to quit") # input is used to hold screen as it takes character
+
+print("Hello world") ; # (;) usage do not create any impact in coding in python
+input("press enter to quit");
+
+print("Hello \nguys") # (\n) is an escape sequence to take cursor on the next line
 input("press enter to quit")
