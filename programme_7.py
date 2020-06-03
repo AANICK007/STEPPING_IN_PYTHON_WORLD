f247f37706632f5ed62971f62d906a7ef29e39e6
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-03 16:06:13 +0530

    Create programme_7.py

---
+++ programme_7.py
@@ -0,0 +1,24 @@
+# This is a programme to study logical operators in python
+
+x = 4 > 3 and 2 > 1 ;
+print ( x ) ;
+x = 4 > 5 and 5 > 1 ;
+print ( x ) ;
+x = 4 > 3 and 3 < 2  ;
+print ( x ) ;
+
+x = 3 > 4 or 3 > 2 ;
+print ( x ) ;
+x = 3 > 4 or 4 > 5 ;
+print ( x ) ;
+
+x = 5 and 4 ;    # if operands are non boolean result is non boolean
+print ( x  ) ;   #  if first operand false result first value and if first value false the value second operand
+
+x = 4 or 3 ;
+print ( x ) ;  # this is vise versa of and operator
+
+x = not 5 ;
+print ( x ) ;
+x = not ( 4 > 5 ) ;
+print ( x ) ;
