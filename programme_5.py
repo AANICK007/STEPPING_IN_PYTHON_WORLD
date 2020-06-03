4e8d025567dad63407a1487d46d57e8ddcbc2ea6
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-03 16:03:56 +0530

    Create programme_5.py

---
+++ programme_5.py
@@ -0,0 +1,53 @@
+# This is a programme to see arithmetic operators in Python
+
+#   (**)   this is exponent operator
+x = 3**2 ;
+print ( x ) ;
+x = 10**-2 ;
+print ( x ) ;
+x = -3**2 ;
+print ( x ) ;
+x = 2.4**128 ;
+print ( x ) ;
+
+#    (/)   this is division operator which gives float value
+x = 3/2 ;
+print ( x ) ;
+
+#    (//)   this is division operator which gives floor value
+x = 3//2 ;
+print ( x ) ;
+x = 3.0//2 ;
+print ( x ) ;
+
+#    (%)    this is modulus operator which gives remainder
+x = 11%7 ;
+print ( x ) ;
+x = 2%3 ;
+print ( x ) ;
+x = 4.5%3 ;
+print ( x ) ;
+x = 2%3.0 ;
+print ( x ) ;
+x = -4 %3 ;
+print ( x ) ;
+x = 4/-3 ;
+print ( x ) ;
+x = -4 % -3 ;
+print ( x ) ;
+
+# (*)    this is multiplication operator
+# (+)    this is addition operator
+# (-)    this is subtraction operator
+x = 11*45 ;
+print ( x ) ;
+x = 3+2 ;
+print ( x ) ;
+x = "abc" ;
+y = "def" ;
+print ( x + y ) ;
+x = "abc" ;
+y = 3 ;
+print ( x*y ) ;
+x = 3456-78963 ;
+print ( x ) ;
