03f8cc74d5c26b3b8486c5e6e68d106e5e667502
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-03 17:34:26 +0530

    Create programme_12.py

---
+++ programme_12.py
@@ -0,0 +1,23 @@
+# This is a programme to take input from user
+
+x = input ( "Enter your name = " ) ;
+print ( x ) ;
+print ( type ( x ) ) ;
+
+x = input ( "Enter a number = " ) ;
+print ( x ) ;
+print ( type ( x ) ) ;
+
+# input takes only string
+
+x = int ( input ( "Enter a number = " ) ) ;
+print ( x ) ;
+print ( type ( x ) ) ;
+
+x = input ( "Enter a complex number = " ) ;
+print ( x ) ;
+print ( type ( x ) ) ;
+
+x = complex ( input ( "Enter a complex number = " ) )  ;
+print ( x ) ;
+print ( type ( x ) ) ;
