f8427d453b064a318af53450c51c9f6852d6bd38
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-03 16:19:47 +0530

    Create programme_8.py

---
+++ programme_8.py
@@ -0,0 +1,19 @@
+# This is a programme to study bitwise operators in python
+
+x =5 & 6 ;   # this is and operator between bit numbers of 5 and 6
+print ( x ) ;
+
+x = 5 | 6 ;
+print ( x ) ;   # this is or operator between bit numbers of 5 and 6
+
+x = ~ 5 ;
+print ( x ) ;    # this is not operator
+
+x = 5 ^ 6 ;
+print ( x ) ;   # this is xor operator between bit numbers of 5 and 6
+
+x = 99 >> 2 ;
+print ( x ) ;
+
+x = 5 << 3 ;
+print ( x ) ;
