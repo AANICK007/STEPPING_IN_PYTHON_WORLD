5ef38236ddeb270d48ff891d9f790a1f237c1647
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-04 06:37:16 +0530

    Update programme_9.py

--- programme_9.py
+++ programme_9.py
@@ -1,19 +1,19 @@
 # This is a programme to study assignment operators in python
 
-x = 5 ;
-print ( x ) ;
+x = 5 ;   
+print ( x ) ; 
 
-x += 45 ;
+x += 45 ;  #  this is x = x + 45
 print ( x ) ;
 
 x =8 ;
-x -= 3 ;
+x -= 3 ;  #  this is x = x - 45
 print ( x ) ;
 
 x = 4 ;
-x *= 4 ;
+x *= 4 ;  #  this is x = x * 45
 print ( x ) ;
 
-x = 90 ;
-x /= 45 ;
+x = 90 ; 
+x /= 45 ;  #  this is x = x / 45
 print ( x ) ;
