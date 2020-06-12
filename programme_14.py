1abed5246205a2b2355d3ee026987573066374f5
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-12 10:48:27 +0530

    Create programme_14.py

---
+++ programme_14.py
@@ -0,0 +1,7 @@
+# Python script to add  two numbers entered by user
+
+x = int ( input ( " Enter the first number = " ) ) ;
+y = int ( input ( " Enter the second number = " ) ) ;
+
+z = x + y ;
+print ( "The sum of the two numbers =", z ) ;
