8d01230cf23dbb4bc73ece1eafff03e1593087a4
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-12 11:07:43 +0530

    Create programme_15.py

---
+++ programme_15.py
@@ -0,0 +1,16 @@
+# Python script to calculate volume of cuboid
+
+l = float ( input ( "Enter the cuboid's length = " ) ) ;
+b = float ( input ( "Enter the cuboid's breadth = " ) ) ;
+h = float ( input ( "Enter the cuboid's height = " ) ) ;
+print ( "The volume of cuboid =" , l*b*h ) ;
+
+
+
+# Python script to calculate simple interest
+
+P = int ( input ( "Enter the total money lended on interest = " ) ) ;
+R = int ( input ( "Enter the rate at which money is lended = " ) ) ;
+t = int ( input ( "Enter the total time for which money lended = " ) ) ;
+
+print ( "The money you will get after" , t , "years as interest is =", (P*R*t)/100 ) ;
