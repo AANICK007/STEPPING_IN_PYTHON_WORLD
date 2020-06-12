650718d230f5236caefab66045200a96d96cd1bd
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-12 11:07:39 +0530

    Update programme_14.py

--- programme_14.py
+++ programme_14.py
@@ -5,3 +5,9 @@ y = int ( input ( " Enter the second number = " ) ) ;
 
 z = x + y ;
 print ( "The sum of the two numbers =", z ) ;
+
+
+# Python script to calculate area of a circle
+
+r = int ( input ( "Enter the circle's radius = " ) ) ;
+print ( "The area of circle =" , 3.14*r*r ) ;
