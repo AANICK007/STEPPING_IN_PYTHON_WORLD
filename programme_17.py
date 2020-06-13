80ff557d93c2443cd6cbc1a57b2cd2571a3ec616
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-13 11:01:52 +0530

    Create programme_17.py

---
+++ programme_17.py
@@ -0,0 +1,11 @@
+# This is a programme to study concept of decision control statement in PYTHON
+
+
+x = int ( input ("Enter a number = " ) )
+
+if (x > 0) :
+    print ("The entered number is positive " )
+elif ( x == 0 ) :
+    print ("The entered number is zero " )
+else :
+    print ("The entered number is negative " ) 
