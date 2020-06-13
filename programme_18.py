36098d204859072c43846a3bb33a5298d30cc9b4
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-13 11:01:56 +0530

    Create programme_18.py

---
+++ programme_18.py
@@ -0,0 +1,19 @@
+# Python script to find if a numberr is even or odd
+
+x = int ( input ("Enter a number = " ) )
+
+if ( x % 2 ) :
+    print ("The number is odd" )
+else :
+    print ("The number is even" )
+    
+    
+    
+# Python script to find if a number is divisible by 5 or not
+
+x = int ( input ("Enter a number = " ) )
+
+if ( x % 5 ) :
+    print ("The number is not divisible by 5" )
+else :
+    print ("The number is divisible by 5" )
