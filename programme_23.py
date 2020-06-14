c01c85a6556b688b941b6ce2881df373c3c649a0
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-14 12:19:57 +0530

    Create programme_23.py

---
+++ programme_23.py
@@ -0,0 +1,18 @@
+# Python script to print first 10 natural numbers
+
+x = 1
+
+while ( x <= 10 ) :
+    print ( x ) ;
+    x += 1 ;
+    
+    
+    
+    
+# Python script to print first 10 natural numbers in reverse order
+
+x = 10
+
+while ( x >= 1 ) :
+    print ( x ) ;
+    x -= 1 ;
