5391a9da6f34ab776b1ad574594f85307b275652
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-14 12:20:05 +0530

    Create programme_25.py

---
+++ programme_25.py
@@ -0,0 +1,21 @@
+# Python script to print first N natural numbers
+
+N = int( input ( "Enter a natural number = " ) )
+x = 1
+
+while ( x <= N ) :
+    print ( x ) ;
+    x += 1 ;
+    
+    
+    
+    
+# Python script to print first N natural numbers in reverse order
+
+N = int( input ( "Enter a natural number = " ) )
+x = N
+
+while ( x >= 1 ) :
+    print ( x ) ;
+    x -= 1 ;
+    
