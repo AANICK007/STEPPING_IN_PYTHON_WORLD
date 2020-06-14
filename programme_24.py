901ce409436753b69bb2b3770c7fbe1eba09fb56
ANIKET PATHAK <aanickpathak@gmail.com>
2020-06-14 12:20:02 +0530

    Create programme_24.py

---
+++ programme_24.py
@@ -0,0 +1,19 @@
+# Python script to print first 10 odd numbers
+
+x = 0
+
+while ( x <= 9 ) :
+    print ( 2*x + 1 ) ;
+    x += 1 ;
+    
+    
+    
+    
+# Python script to print first 10 even numbers
+
+x = 0
+
+while ( x <= 9 ) :
+    print ( 2*x ) ;
+    x += 1 ;
+    
